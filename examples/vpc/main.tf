e6b06a92f0e1a2ca4689b81288c71c6a6339f000
Luis M. Gallardo D <lgallard@gmail.com>
2020-04-06 17:52:13 -0300

    Add vpc example

---
+++ examples/vpc/main.tf
@@ -0,0 +1,75 @@
+# CodeBuild
+module "myapp-project" {
+  source = "../../terraform-aws-codebuild"
+
+  name        = "my-app"
+  description = "Codebuild for deploying myapp"
+
+  codebuild_source_version = "master"
+  codebuild_source = {
+    type            = "GITHUB"
+    location        = "https://github.com/mitchellh/packer.git"
+    git_clone_depth = 1
+
+    git_submodules_config = {
+      fetch_submodules = true
+    }
+  }
+
+  environment = {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:2.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+
+    # Environment variables
+    variables = [
+      {
+        name  = "KEY1"
+        value = "VALUE1"
+      },
+      {
+        name  = "KEY2"
+        value = "VALUE2"
+      },
+    ]
+  }
+
+  artifacts = {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache = {
+    type     = "S3"
+    location = aws_s3_bucket.myapp-project.bucket
+  }
+
+  # Logs
+  s3_logs = {
+    status   = "ENABLED"
+    location = "${aws_s3_bucket.myapp-project.id}/build-log"
+  }
+
+
+  # VPC
+  vpc_config = {
+    vpc_id             = "vpc-123446789101"
+    subnets            = ["subnet-7a1dc5a54444", "subnet-6b4a45b64444"]
+    security_group_ids = ["sg-b475b46c4444", "sg-58b61a4c4444"]
+
+  }
+
+  # Tags
+  tags = {
+    Environment = "dev"
+    owner       = "lgallard"
+  }
+
+}
+
+# S3
+resource "aws_s3_bucket" "myapp-project" {
+  bucket = "myapp-project-bucket"
+  acl    = "private"
+}
+
